05ac32bbd9f758d4291eaa4f8debf6c3a6a7facf
Rob Lark <quietcalmrecords@Robs-iMac.local>
2020-08-07 16:49:58 -0700

    continued styling

--- user-card/src/Components/UserCard.js
+++ user-card/src/Components/UserCard.js
@@ -1,5 +1,21 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+
+const styles = (theme) => ({
+	root: {
+        flexGrow: 1,
+        display: 'flex',
+        justifyContent: "center",
+        
+	},
+	avatar: {
+		border: "2px solid black",
+        borderRadius: "250px",
+        height: "13em"
+	},
+});
 
 class UserCard extends React.Component {
 	constructor() {
@@ -11,13 +27,31 @@ class UserCard extends React.Component {
 
 	componentDidMount() {
 		axios
-			.get("https://api.github.com/users/Robert-Lark")
+			.get("https://api.github.com/users/robert-lark/followers")
 			.then((res) => {
-				this.setState({ users: res });
-				console.log(res);
+				this.setState({ users: res.data });
+				console.log(this.state.users);
 			})
 			.catch((err) => console.log(err));
 	}
+
+	render() {
+		const { classes } = this.props;
+		return (
+			<div className={classes.root}>
+				{this.state.users.map((users) => (
+					<Grid item xs={3}>
+						<img
+							className={classes.avatar}
+                            alt="avatar"
+                            key={users}
+							src={users.avatar_url}
+						/>
+					</Grid>
+				))}
+			</div>
+		);
+	}
 }
 
-export default UserCard
+export default withStyles(styles)(UserCard);
